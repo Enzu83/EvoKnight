61e3d25837c49b5514834b35be83d0649710c20c
Maxime Chambre <enzu.mc@gmail.com>
2025-02-19 11:38:01 +0100

    finish pause menu

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
+@onready var cursor: AnimatedSprite2D = $Menu/Cursor
+@onready var select_sound: AudioStreamPlayer = $Menu/SelectSound
+@onready var resume_button: Label = $Menu/ResumeButton
+@onready var quit_button: Label = $Menu/QuitButton
 
 func _ready() -> void:
 	visible = false
@@ -13,7 +17,23 @@ func _process(_delta: float) -> void:
 		handle_menu()
 
 func handle_menu() -> void:
-	pass
+	if (Input.is_action_just_pressed("basic_slash") \
+	or Input.is_action_just_pressed("dash") \
+	or Input.is_action_just_pressed("magic_slash")):
+		if cursor.position.y == resume_button.position.y + 10:
+			toggle_pause()
+		elif cursor.position.y == quit_button.position.y + 10:
+			get_tree().quit()
+	
+	elif Input.is_action_just_pressed("up") \
+	and cursor.position.y != resume_button.position.y + 10:
+		cursor.position.y = resume_button.position.y + 10
+		select_sound.play()
+	
+	elif Input.is_action_just_pressed("down") \
+	and cursor.position.y != quit_button.position.y + 10:
+		cursor.position.y = quit_button.position.y + 10
+		select_sound.play()
 
 func toggle_pause() -> void:
 	visible = !visible
