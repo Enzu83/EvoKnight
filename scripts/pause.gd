4b3e9b39cf229c8cddd6e1ab9a2b9982280c3a2f
Maxime Chambre <enzu.mc@gmail.com>
2025-02-19 13:16:54 +0100

    refactore pause code

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -5,8 +5,13 @@ extends CanvasLayer
 @onready var resume_button: Label = $Menu/ResumeButton
 @onready var quit_button: Label = $Menu/QuitButton
 
+enum State {Resume, Quit}
+
+var state: State
+
 func _ready() -> void:
 	visible = false
+	state = State.Resume
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause") and Global.current_level != 0:
@@ -17,24 +22,38 @@ func _process(_delta: float) -> void:
 		handle_menu()
 
 func handle_menu() -> void:
+	# cursor position
+	if state == State.Resume:
+		cursor.position.y = resume_button.position.y + 10
+	elif state == State.Quit:
+		cursor.position.y = quit_button.position.y + 10
+	
+	# select an option
 	if (Input.is_action_just_pressed("basic_slash") \
 	or Input.is_action_just_pressed("dash") \
 	or Input.is_action_just_pressed("magic_slash")):
-		if cursor.position.y == resume_button.position.y + 10:
+		if state == State.Resume:
 			toggle_pause()
-		elif cursor.position.y == quit_button.position.y + 10:
+		elif state == State.Quit:
 			get_tree().quit()
 	
+	# choose option
 	elif Input.is_action_just_pressed("up") \
-	and cursor.position.y != resume_button.position.y + 10:
-		cursor.position.y = resume_button.position.y + 10
+	and state == State.Quit:
+		state = State.Resume
 		select_sound.play()
 	
 	elif Input.is_action_just_pressed("down") \
-	and cursor.position.y != quit_button.position.y + 10:
-		cursor.position.y = quit_button.position.y + 10
+	and state == State.Resume:
+		state = State.Quit
 		select_sound.play()
 
 func toggle_pause() -> void:
-	visible = !visible
-	get_tree().paused = !get_tree().paused
+	state = State.Resume
+	
+	if not visible:
+		visible = true
+		get_tree().paused = true	
+	else:
+		visible = false
+		get_tree().paused = false
