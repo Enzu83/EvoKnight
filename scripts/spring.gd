b8ceb477c861e26617a3041fc5d3fdf27d0eebab
Maxime Chambre <enzu.mc@gmail.com>
2025-03-04 14:59:34 +0100

    add 2 stars in level 4 & fix spring bug (player could activate the spring through a wall of size 1)

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -3,36 +3,54 @@ extends Area2D
 @onready var player: Player = %Player
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hitbox: CollisionShape2D = $Hitbox
 
 const SPRING_FORCE = 450.0
 
-var direction: Vector2
+var orientation: String
+var launch_direction: Vector2
 
 func spring(body: Node2D) -> void:
 	animation_player.stop()
 	animation_player.play("spring")
-	body.bumped(SPRING_FORCE, direction)
+	body.bumped(SPRING_FORCE, launch_direction)
 
 func _ready() -> void:
 	# bump up
 	if posmod(round(rotation_degrees), 360) == 0:
-		direction = Vector2.UP
+		orientation = "up"
+		launch_direction = Vector2.UP
 	# bump right
 	elif posmod(round(rotation_degrees), 360) == 90:
-		direction = Vector2(1, -0.4)
+		orientation = "right"
+		launch_direction = Vector2(1, -0.4)
 	# bump down
 	elif posmod(round(rotation_degrees), 360) == 180:
-		direction = Vector2.DOWN
+		orientation = "down"
+		launch_direction = Vector2.DOWN
 	# bump left
 	elif posmod(round(rotation_degrees), 360) == 270:
-		direction = Vector2(-1, -0.4)
-	
-func _on_area_entered(area: Area2D) -> void:
-	var body := area.get_parent()
-	
+		orientation = "left"
+		launch_direction = Vector2(-1, -0.4)
+
+func _physics_process(_delta: float) -> void:
+	# deactivate hitbox if player is behind the spring
+	if (orientation == "up" and player.position.y - position.y > 8) \
+	or (orientation == "right" and player.position.x - position.x < -8) \
+	or (orientation == "down" and player.position.y - position.y < -8) \
+	or (orientation == "left" and player.position.x - position.x > 8):
+		hitbox.disabled = true
+	else:
+		hitbox.disabled = false
+
+func check_spring(body: Node2D) -> void:
 	if body == player and body.state != body.State.Fainted:
 		spring(body)
 
+func _on_area_entered(area: Area2D) -> void:
+	var body := area.get_parent()
+	
+	check_spring(body)
+	
 func _on_body_entered(body: Node2D) -> void:
-	if body == player and body.state != body.State.Fainted:
-		spring(body)
+	check_spring(body)
