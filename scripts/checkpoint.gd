9dc5d6a310d258c378998c24436da2cb3e1f9f92
Maxime Chambre <enzu.mc@gmail.com>
2025-03-18 02:54:20 +0100

    continue level 3 design & can collect pending stars on checkpoint even if it's green

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -1,19 +1,38 @@
 extends Area2D
 
-@onready var sprite: Sprite2D = $Sprite
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var get_sound: AudioStreamPlayer = $GetSound
 
-var red_sprite := preload("res://assets/sprites/items/spr_flag_red.png")
-var green_sprite := preload("res://assets/sprites/items/spr_flag_green.png")
+enum Anim {red, green, yellow}
+var anim: Anim
 
-func _process(_delta: float) -> void:
-	if Global.respawn_position == position:
-		sprite.texture = green_sprite
+func play_animation(anim_name: String) -> void:
+	# play the animation if it's not the current one
+	if anim != Anim.get(anim_name):
+		animation_player.play(anim_name)
+		anim = Anim.get(anim_name)
+
+func _physics_process(_delta: float) -> void:
+	if Global.respawn_position != position:
+		play_animation("red")
+	elif Global.get_pending_stars() > 0:
+		play_animation("yellow")
 	else:
-		sprite.texture = red_sprite
+		play_animation("green")
 
-func _on_body_entered(body: Node2D) -> void:
-	if body == Global.player and Global.respawn_position != position:
+func collect_checkpoint() -> void:
+	if Global.respawn_position != position \
+	or Global.get_pending_stars() > 0:
 		Global.respawn_position = position
 		Global.collect_pending_stars()
 		get_sound.play()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == Global.player:
+		collect_checkpoint()
+
+func _on_area_entered(area: Area2D) -> void:
+	var body := area.get_parent()
+	
+	if body == Global.player:
+		collect_checkpoint()
